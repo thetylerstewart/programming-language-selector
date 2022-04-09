2e79717005f88db74adb8a5865e817dfc4a705ad
Tyler Stewart <mr.tstew@gmail.com>
2022-04-08 17:02:05 -0700

    add refactored code

--- js/scripts.js
+++ js/scripts.js
@@ -1,150 +1,48 @@
+var x = 0;
 $(document).ready(function () {
-  $('form#form-one').submit(function (event) {
+  $('form').submit(function (event) {
     event.preventDefault();
+    x = x + 1;
     $('input:radio').each(function () {
       if ($(this).is(':checked')) {
-        const formOne = $(this).val();
 
-        if (formOne === 'a') {
+        if ($(this).val() === 'a') {
           $('.language-one').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-one').hide();
-          $('.jumbo-two').show();
-
-        } else if (formOne === 'b') {
+        } else if ($(this).val() === 'b') {
           $('.language-two').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-one').hide();
-          $('.jumbo-two').show();
-
-        } else if (formOne === 'c') {
+        } else if ($(this).val() === 'c') {
           $('.language-three').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-one').hide();
-          $('.jumbo-two').show();
 
-        } else if (formOne === 'd') {
+        } else if ($(this).val() === 'd') {
           $('.language-four').css('color', 'red').css('border', '1px solid red').fadeOut(1200)
-          $('.jumbo-one').hide();;
-          $('.jumbo-two').show();
-
         }
       }
-    })
-  })
-})
-
-$(document).ready(function () {
-  $('form#form-two').submit(function (event) {
-    event.preventDefault();
-    $('input:radio').each(function () {
-      if ($(this).is(':checked')) {
-        const formTwo = $(this).val();
-
-        if (formTwo === 'a') {
-          $('.language-one').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-two').hide();
-          $('.jumbo-three').show();
-
-        } else if (formTwo === 'b') {
-          $('.language-two').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-two').hide();
-          $('.jumbo-three').show();
-
-        } else if (formTwo === 'c') {
-          $('.language-three').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-two').hide();
-          $('.jumbo-three').show();
-
-        } else if (formTwo === 'd') {
-          $('.language-four').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-two').hide();
-          $('.jumbo-three').show();
-        }
+      if (x === 1) {
+        $('.jumbo-one').hide();
+        $('.jumbo-two').show();
+      } else if (x === 2) {
+        $('.jumbo-two').hide();
+        $('.jumbo-three').show();
       }
-    })
-  })
-})
-
-$(document).ready(function () {
-  $('form#form-three').submit(function (event) {
-    event.preventDefault();
-    $('input:radio').each(function () {
-      if ($(this).is(':checked')) {
-        const formThree = $(this).val();
-
-        if (formThree === 'a') {
-          $('.language-one').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-three').hide();
-          $('.jumbo-four').show();
-
-        } else if (formThree === 'b') {
-          $('.language-two').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-three').hide();
-          $('.jumbo-four').show();
-
-        } else if (formThree === 'c') {
-          $('.language-three').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-three').hide();
-          $('.jumbo-four').show();
-
-        } else if (formThree === 'd') {
-          $('.language-four').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-three').hide();
-          $('.jumbo-four').show();
-        }
+      else if (x === 3) {
+        $('.jumbo-three').hide();
+        $('.jumbo-four').show();
       }
-    })
-  })
-})
-
-$(document).ready(function () {
-  $('form#form-four').submit(function (event) {
-    event.preventDefault();
-    $('input:radio').each(function () {
-      if ($(this).is(':checked')) {
-        const formFour = $(this).val();
-
-        if (formFour === 'a') {
-          $('.language-one').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-four').hide();
-          $('.jumbo-five').show();
-
-        } else if (formFour === 'b') {
-          $('.language-two').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-four').hide();
-          $('.jumbo-five').show();
-
-        } else if (formFour === 'c') {
-          $('.language-three').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-four').hide();
-          $('.jumbo-five').show();
-
-        } else if (formFour === 'd') {
-          $('.language-four').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-four').hide();
-          $('.jumbo-five').show();
-        }
+      else if (x === 4) {
+        $('.jumbo-four').hide();
+        $('.jumbo-five').show();
       }
-    })
-  })
-})
-
-$(document).ready(function () {
-  $('form#form-five').submit(function (event) {
-    event.preventDefault();
-    $('input:radio').each(function () {
-      if ($(this).is(':checked')) {
-        const formFive = $(this).val();
-
-        if ((formFive === 'a') || (formFive === 'b') || (formFive === 'c') || (formFive === 'd')) {
-          $('.language-one').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.language-two').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.language-three').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.language-four').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-          $('.jumbo-five').hide();
-          $('.success').show();
-          $('.navbar').css('color', 'green').css('border', '1px solid green').show();
-          $('.email-collection').show();
-          $('.header-question').hide();
-        }
+      else if (x === 5) {
+        $('.jumbo-five').hide();;
+        $('.language-one').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
+        $('.language-two').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
+        $('.language-three').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
+        $('.language-four').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
+        $('.jumbo-five').hide();
+        $('.success').show();
+        $('.navbar').css('color', 'green').css('border', '1px solid green').show();
+        $('.email-collection').show();
+        $('.header-question').hide();
       }
     })
   })
