59f558e40e560fc45df6e51818006159e3905442
Tyler Stewart <mr.tstew@gmail.com>
2022-04-09 17:02:56 -0700

    add title.. oops

--- js/scripts.js
+++ js/scripts.js
@@ -16,31 +16,31 @@ $(document).ready(function () {
         } else if ($(this).val() === 'd') {
           $('.language-four').css('color', 'red').css('border', '1px solid red').fadeOut(1200)
         }
-      }
 
-      if (x === 1) {
-        $('.jumbo-one').hide();
-        $('.jumbo-two').show();
-      } else if (x === 2) {
-        $('.jumbo-two').hide();
-        $('.jumbo-three').show();
-      } else if (x === 3) {
-        $('.jumbo-three').hide();
-        $('.jumbo-four').show();
-      } else if (x === 4) {
-        $('.jumbo-four').hide();
-        $('.jumbo-five').show();
-      } else if (x === 5) {
-        $('.jumbo-five').hide();;
-        $('.language-one').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-        $('.language-two').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-        $('.language-three').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-        $('.language-four').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
-        $('.jumbo-five').hide();
-        $('.success').show();
-        $('.navbar').css('color', 'green').css('border', '3px solid green').show();
-        $('.email-collection').show();
-        $('.header-question').hide();
+        if (x === 1) {
+          $('.jumbo-one').hide();
+          $('.jumbo-two').show();
+        } else if (x === 2) {
+          $('.jumbo-two').hide();
+          $('.jumbo-three').show();
+        } else if (x === 3) {
+          $('.jumbo-three').hide();
+          $('.jumbo-four').show();
+        } else if (x === 4) {
+          $('.jumbo-four').hide();
+          $('.jumbo-five').show();
+        } else if (x === 5) {
+          $('.jumbo-five').hide();;
+          $('.language-one').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
+          $('.language-two').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
+          $('.language-three').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
+          $('.language-four').css('color', 'red').css('border', '1px solid red').fadeOut(1200);
+          $('.jumbo-five').hide();
+          $('.success').show();
+          $('.navbar').css('color', 'green').css('border', '3px solid green').show();
+          $('.email-collection').show();
+          $('.header-question').hide();
+        }
       }
     })
   })
